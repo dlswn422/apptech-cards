a883866b5765c5350dc6333948a11af7f29dc99a
INJU MOON <injoo.mun@jkax.co.kr>
2026-01-27 11:27:34 +0900

    - 모바일 UI 개선

--- components/AppCardList.tsx
+++ components/AppCardList.tsx
@@ -16,7 +16,7 @@ export default function AppCardList() {
   return (
     <div>
       {/* 카드 리스트 */}
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
         {currentApps.map((app) => (
           <AppCard key={app.id} app={app} />
         ))}
